04c3d5582d1a972abf042a44cf61b9df28550327
SwiftCris <cristicristi20110906@gmail.com>
2025-10-27 16:41:03 +0200

    e

--- mods/greatWeapons.js
+++ mods/greatWeapons.js
@@ -1,10 +1,4 @@
 elements.nuke = {
-  tick: function(pixel) {
-    if (pixel.temp >= 50) {
-      alert("test");
-    }
-
-  },
   color: "#yellow",
   behaviour: behaviours.SOLID,
   reactions: {
@@ -13,6 +7,12 @@ elements.nuke = {
   burn: 50,
   category: "goodWeapons",
   state: "solid",
+  tick: function(pixel) {
+    if (pixel.temp >= 50) {
+      alert("test");
+    }
+
+  }
 
 
-}
+};
