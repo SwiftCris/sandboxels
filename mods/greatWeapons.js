53376c368ca0e8f28a516d4b448740e12739ff63
SwiftCris <cristicristi20110906@gmail.com>
2025-10-28 09:37:14 +0200

    e

--- mods/greatWeapons.js
+++ mods/greatWeapons.js
@@ -1,14 +1,19 @@
 elements.nuke = {
   color: "#FFFF00",
-  behavior: behaviors.WALL,
+  behavior: [
+    "XX|EX:10>ash|XX",
+    "XX|XX|XX",
+    "M2|M1|M2",
+  ],
   reactions: {
-    "heat": { "elem2": null }
+    "heat": { "elem2": null },
   },
   burn: 50,
   category: "goodWeapons",
   state: "solid",
   tick: function(pixel) {
     if (pixel.temp >= 50) {
+
       alert("test");
     }
 
